868a677ce84221d6cb8a37e5f54fa50e46c74840
Nikita Novoselov <sarkomath@outlook.com>
2021-04-28 22:03:46 +1000

    - update Selenide. - formatting data-gen logic. - formatting test logic.

--- src/test/java/MapDeliveryTest.java
+++ src/test/java/MapDeliveryTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
 import java.text.DateFormat;
@@ -14,25 +15,26 @@ import static com.codeborne.selenide.Condition.*;
 
 public class MapDeliveryTest {
 
-    String getDate(int currentDate) {
+    String getDate(int daysFromCurrentDate) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, 5);
+        cal.add(Calendar.DATE, daysFromCurrentDate);
         return dateFormat.format(cal.getTime());
     }
 
     @Test
     // Full Positive Test
     void fullPositiveTest() {
-        Configuration.timeout = 60000;
+        // Configuration.timeout = 60000;
 
         open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue("Хабаровск"); // Устанавливаем правильный город
-        $("[placeholder='Дата встречи']").setValue(getDate(2)); // вводим правильный формат даты
-        $("[name='name']").setValue("Иванов Иван"); // ВВодим правильные ИФ
-        $("[name='phone']").setValue("+70009998877"); // Вводим правильным номер телефона
+        $("[placeholder='Город']").setValue("Хабаровск");
+        $("[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(getDate(5));
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+70009998877");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(byText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible);
+        $(byText("Успешно!")).waitUntil(visible, 15000);
     }
 }
